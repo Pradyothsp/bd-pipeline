50ffcec0b93dccef2e35ca5601c254b914820a11
Pradyoth S P <pradyothsp@Pradyoths-MacBook-Air.local>
2023-11-14 12:53:22 -0500

    Restructure

--- settings.py
+++ settings.py
@@ -3,19 +3,6 @@ TOPIC = 'csv-event-data'
 SERVER_URL = 'localhost:9092'  # Kafka server URL
 
 
-# CONSUMER SETTINGS
-# Configure the Kafka consumer
-CONSUMER_GROUP_ID = 'csv-event-consumer-group'
-
-CONSUMER_CONF = {
-    'bootstrap.servers': SERVER_URL,
-    'group.id': CONSUMER_GROUP_ID,  # Choose a consumer group ID
-    'auto.offset.reset': 'earliest'
-}
-
-KAFKA_OUPPUT_FOLDER = 'kafka_output'
-
-
 # PRODUCER SETTINGS
 # Configure the Kafka producer
 PRODUCER_CONF = {
@@ -28,3 +15,16 @@ INPUT_CSV_PATH = ['data/data_2019_Oct.csv', 'data/data_2019_Nov.csv']
 
 # Set the batch size
 BATCH_SIZE = 5000  # Adjust as needed
+
+
+# CONSUMER SETTINGS
+# Configure the Kafka consumer
+CONSUMER_GROUP_ID = 'csv-event-consumer-group'
+
+CONSUMER_CONF = {
+    'bootstrap.servers': SERVER_URL,
+    'group.id': CONSUMER_GROUP_ID,  # Choose a consumer group ID
+    'auto.offset.reset': 'earliest'
+}
+
+KAFKA_OUPPUT_FOLDER = 'kafka_output'
